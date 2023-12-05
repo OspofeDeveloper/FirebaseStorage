21341825ef06fc9a38403895ba0ef5b294ae432a
OspofeDeveloper <ospofedeveloper@gmail.com>
2023-12-05 19:10:26 +0100

    add name to file with compose implemented

--- app/src/main/java/com/example/firebasestorage/ui/compose/upload/UploadComposeActivity.kt
+++ app/src/main/java/com/example/firebasestorage/ui/compose/upload/UploadComposeActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.result.contract.ActivityResultContracts.TakePicture
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,12 +20,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults.textFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -32,13 +36,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.core.content.FileProvider
@@ -77,11 +85,19 @@ class UploadComposeActivity : AppCompatActivity() {
         var showImageDialog: Boolean by remember { mutableStateOf(false) }
         var resultUri: Uri? by remember { mutableStateOf(null) }
         val loading by uploadComposeViewModel.isLoading.collectAsState()
+        var userTitle: String by remember { mutableStateOf("") }
+
+        val focusRequester = remember { FocusRequester() }
+        val focusManager = LocalFocusManager.current
 
         val intentCameraLauncher =
             rememberLauncherForActivityResult(TakePicture()) {
                 if (it && uri?.path?.isNotEmpty() == true) { //Si da success y path de la uri no es nulo
-                    uploadComposeViewModel.uploadAndGetImage(uri!!) { newUri -> resultUri = newUri }
+                    uploadComposeViewModel.uploadAndGetImage(uri!!) { newUri ->
+                        userTitle = ""
+                        focusManager.clearFocus()
+                        resultUri = newUri
+                    }
                 }
             }
 
@@ -97,7 +113,7 @@ class UploadComposeActivity : AppCompatActivity() {
                     Column(modifier = Modifier.padding(24.dp)) {
                         OutlinedButton(
                             onClick = {
-                                uri = generateUri()
+                                uri = generateUri(userTitle)
                                 intentCameraLauncher.launch(uri)
                                 showImageDialog = false
                             },
@@ -141,7 +157,11 @@ class UploadComposeActivity : AppCompatActivity() {
                     .padding(horizontal = 36.dp)
             ) {
                 if (resultUri != null) {
-                    AsyncImage(model = resultUri, contentDescription = "image selected by user", contentScale = ContentScale.Crop)
+                    AsyncImage(
+                        model = resultUri,
+                        contentDescription = "image selected by user",
+                        contentScale = ContentScale.Crop
+                    )
                 }
 
                 if (loading) {
@@ -153,10 +173,10 @@ class UploadComposeActivity : AppCompatActivity() {
                         )
                     }
                 }
-                
-                if(!loading && resultUri == null) {
+
+                if (!loading && resultUri == null) {
                     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        Icon (
+                        Icon(
                             painter = painterResource(id = R.drawable.ic_upload_image),
                             contentDescription = null,
                             modifier = Modifier.size(100.dp),
@@ -165,6 +185,27 @@ class UploadComposeActivity : AppCompatActivity() {
                     }
                 }
             }
+            Spacer(modifier = Modifier.weight(1f))
+            TextField(
+                value = userTitle, onValueChange = { userTitle = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 36.dp)
+                    .border(2.dp, colorResource(id = R.color.green), RoundedCornerShape(22))
+                    .focusRequester(focusRequester),
+                colors = textFieldColors(
+                    backgroundColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                )
+            )
+
             Spacer(modifier = Modifier.weight(1f))
             Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
                 FloatingActionButton(onClick = {
@@ -182,16 +223,17 @@ class UploadComposeActivity : AppCompatActivity() {
         }
     }
 
-    private fun generateUri(): Uri {
+    private fun generateUri(userTitle: String): Uri {
         return FileProvider.getUriForFile(
             Objects.requireNonNull(this),
             "com.example.firebasestorage.provider",
-            createFile()
+            createFile(userTitle)
         )
     }
 
-    private fun createFile(): File {
-        val name = SimpleDateFormat("yyyyMMdd_hhmmss").format(Date()) + "image"
+    private fun createFile(userTitle: String): File {
+        val name =
+            userTitle.ifEmpty { SimpleDateFormat("yyyyMMdd_hhmmss").format(Date()) + "image" }
         return File.createTempFile(name, ".jpg", externalCacheDir)
     }
 }
